bf7f0fc7285227aa5149ba971d6a672ab143badd
Denis Shafarenko <phisika001@mail.ru>
2023-05-01 01:23:26 +0300

    add gui (main menu and pause menu)

---
+++ gui/main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_start_button_pressed():
+	pass # get_tree().change_scene_to_file("res://world.tscn")
+
+
+func _on_options_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
